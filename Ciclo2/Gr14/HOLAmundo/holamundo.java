ae87d06945a1cf9ba893609c4859edb94586f1e5
GmorG123 <emanuelmor123@gmail.com>
2022-07-05 14:43:16 -0500

    variables

--- Ciclo2/Gr14/HOLAmundo/holamundo.java
+++ Ciclo2/Gr14/HOLAmundo/holamundo.java
@@ -4,5 +4,12 @@ public class holamundo{
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("segundo commit");
+        // variables
+        int num = 1;
+        double dec = 1.5;
+        boolean flag = true;
+        String str = "cadena de texto";
+        System.out.println("numero: " + num + " decimal: " +dec+ " boolean: " +flag+ " string: " + str);
+
     }
 }
