f346a658909c6466e2c19d72ecc8480b721e7501
GmorG123 <emanuelmor123@gmail.com>
2022-07-05 14:16:45 -0500

    ejercicio 1

---
+++ Ciclo2/Gr14/HOLAmundo/holamundo.java
@@ -0,0 +1,8 @@
+package HOLAmundo;
+
+public class holamundo{
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("segundo commit");
+    }
+}
