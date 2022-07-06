6b0cb019961b38f1cac19e0eeb8c4f85a6884cd5
GmorG123 <emanuelmor123@gmail.com>
2022-07-05 23:41:22 -0500

    eje 1 resuelto

--- Ciclo2/Gr14/HOLAmundo/Clase2.java
+++ Ciclo2/Gr14/HOLAmundo/Clase2.java
@@ -1,5 +1,8 @@
 package HOLAmundo;
 
+import java.util.Scanner;
+
+
 public class Clase2 {
     public static void main(String[] args) {
         String mensaje = "Jello mundo";
@@ -10,5 +13,72 @@ public class Clase2 {
         //capturar un caracter a apartir de su posicion
         char chr = mensaje.charAt(4);
         System.out.println("caracter en pos 4: " + chr);
+
+        
+        math_library();
+        eje1_num_suerte();
+    }
+
+    public static void math_library(){
+        double square_root = Math.sqrt(2);
+        System.out.println("raiz cuadrada: " + square_root);
+
+        //power
+        double potencia = Math.pow(4, 10);
+        System.out.println("4 a la 10: " + potencia);
+
+         
+    }
+
+    public static void eje1_num_suerte(){
+        System.out.println("Ingrese su fecha de nacimiento:");
+        Scanner sc = new Scanner(System.in);
+        //Scanner mes = new Scanner(System.in);
+        //Scanner year = new Scanner(System.in);
+        
+        System.out.print("dia: ");
+        int dia = sc.nextInt();
+        System.out.print("mes: ");
+        int mes = sc.nextInt();
+        System.out.print("anio: ");
+        int anio = sc.nextInt();
+
+        sc.close();
+
+        int suma = dia + mes + anio;
+
+        String s = Integer.toString(suma);
+        //String s = String.valueOf(suma);
+
+        int[] num_array = new int[s.length()];
+
+        for(int i=0; i<s.length(); i++){
+            num_array[i] = s.charAt(i) - '0';
+        }
+        suma = 0;
+        for(int i=0; i< num_array.length; i++){
+            suma += num_array[i];
+        }
+
+        System.out.println(suma);
+        
+        /*
+         * Solution 2 con una funcion y trucazos de strings
+        */
+        
+        //suma fecha nacimiento
+        String suma_str = "" +(dia + mes + anio); 
+        
+        //CharAt(i) -> retorna carater de la pos i
+        int num_suerte = 0;
+        //num_suerte = str_to_int(suma_str.charAt(0)) + str_to_int(suma_str.charAt(1)) + str_to_int(suma_str.charAt(2)) + str_to_int(suma_str.charAt(3));
+        for(int i=0; i<suma_str.length(); i++){
+            num_suerte += str_to_int(suma_str.charAt(i));
+        }
+        System.out.println("el numero de suerte es: " + num_suerte);
+    }
+
+    public static int str_to_int(char caracter){
+        return Integer.parseInt(""+ caracter);
     }
 }
