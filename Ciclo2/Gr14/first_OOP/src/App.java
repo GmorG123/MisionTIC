839df945f057517eec53c5463015bc40d7f04faa
GmorG123 <emanuelmor123@gmail.com>
2022-07-07 17:59:36 -0500

    OOP

--- Ciclo2/Gr14/first_OOP/src/App.java
+++ Ciclo2/Gr14/first_OOP/src/App.java
@@ -2,6 +2,27 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         //crear - instanciar objeto
-        new Persona("Gian","Morales");
+        Persona P1 = new Persona("Gian","Morales");
+        //acceder a nombre y apellido
+        String name_persona = P1.getNombre();
+        String lastname_persona = P1.getApellido();
+
+        System.out.println("Nombre: "+name_persona+ "\nApellido: "+ lastname_persona);
+        
+        Persona P2 = new Persona("Emanuel", "Gonzalez");
+        System.out.println(P2.getNombre() +" "+ P2.getApellido());
+        P2.setNombre("Pablo");
+        P2.setApellido("Montoya");
+        System.out.println(P2.getNombre() +" "+ P2.getApellido());
+    
+        //asignar edad P1 Y P2
+        P1.setEdad(22);
+        P2.setEdad(25);
+
+        //calcular num suerte
+        int n_suerte1 = P1.calc_num_suerte(10, 2);
+        int n_suerte2 = P2.calc_num_suerte(30, 3);
+        System.out.println("num suerte de " + P1.getNombre() + " es " + n_suerte1);
+        System.out.println("num suerte de " + P2.getNombre() + " es " + n_suerte2);
     }
 }
--- Ciclo2/Gr14/first_OOP/src/Persona.java
+++ Ciclo2/Gr14/first_OOP/src/Persona.java
@@ -2,9 +2,10 @@
  * Persona
  */
 public class Persona {
-    //Atributos
+    //Atributos (generalmente privados)
     private String nombre;
     private String apellido;
+    private int edad;
 
     //metodo Constructor
     public Persona(String nombre, String apellido){
@@ -12,6 +13,34 @@ public class Persona {
         // nombre referencia a el parametro del constructor
         this.nombre = nombre;
         this.apellido = apellido;
-        System.out.println("Hola personA, mi nombre es: " + nombre +" " + apellido);
+        //System.out.println("Hola personA, mi nombre es: " + nombre +" " + apellido);
+    }
+
+    //metodos consultores (generalmente publicos)
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellido(){
+        return apellido; 
+    }
+    public int getEdad(){
+        return edad;
+    }
+
+    //metodos modificadores
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    //Acciones - Comportamientos
+    public int calc_num_suerte(int dia, int mes){
+        int num_suerte = (edad * mes) / dia;
+        return num_suerte; 
     }
 }
